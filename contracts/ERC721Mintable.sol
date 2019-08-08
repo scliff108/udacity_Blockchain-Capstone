8eb665a73c69498be7a72398e2c42dc2a994ca4e
Shamus Clifford <shamusclifford@bpsma.org>
2019-08-08 15:28:13 -0400

    Complete ERC721Mintable Contract

--- contracts/ERC721Mintable.sol
+++ contracts/ERC721Mintable.sol
@@ -533,12 +533,28 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
 }
 
-//  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
-//  1) Pass in appropriate values for the inherited ERC721Metadata contract
-//      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
-//  2) create a public mint() that does the following:
-//      -can only be executed by the contract owner
-//      -takes in a 'to' address, tokenId, and tokenURI as parameters
-//      -returns a true boolean upon completion of the function
-//      -calls the superclass mint and setTokenURI functions
+contract ERC721Mintable is ERC721Metadata("CliffCoin", "CFC", "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
+
+    address private owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        _;
+    }
+
+    /**
+     * @dev Mint new token
+     * @param to address of the owner of the newly minted token
+     * @param tokenId id of the newly minted token
+     */
+    function mint(address to, uint256 tokenId) public onlyOwner returns(bool) {
+        super._mint(to, tokenId);
+        super.setTokenURI(tokenId);
+        return true;
+    }
+
+}
 
